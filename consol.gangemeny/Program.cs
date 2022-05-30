5f55d5a168299d923986e0a7df4fce16aef0e1ea
tomas <tomas@DESKTOP-MV6VH7K>
2022-05-30 12:32:20 +0200

    Add project files.

---
+++ consol.gangemeny/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace consol.gangemeny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");        }
+    }
+}
---
+++ consol.gangemeny/meny.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consol.gangemeny
+{
+    class meny
+    {
+        /*Lag et konsoll-applikasjon som starter med tallet 1.
+        Menyvalgene du har er:
+         d - doble tallet
+         +  - øke med 1
+         r   - reset til 1*/
+
+        //propertes
+
+        //constructor
+        //Metheds
+
+    }
+}
